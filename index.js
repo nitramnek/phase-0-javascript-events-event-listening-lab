fe9d13b4b4375c99ecfac116d13504d591e99167
Kenneth <nitramnek@gmail.com>
2023-03-27 19:14:09 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//K3n5c0d3!
+
 function addingEventListener() {
-}
+        const input = document.getElementById('input');
+
+        function clickAlert() {
+                alert('I was clicked!');
+            }
+        input.addEventListener('click', clickAlert);
+    }
